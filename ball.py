88733e837a807e6ed0b951c436c7dade42006117
Rah <arrehman757@gmail.com>
2023-11-22 16:50:31 +0530

    Added ball out of bounds - resets ball spawn

--- ball.py
+++ ball.py
@@ -12,13 +12,23 @@ class Ball(Turtle):
         self.penup()
         self.xMovement = 10
         self.yMovement = 10
+        self.movementDirection = 1
     
     def move(self):
-        new_x = self.xcor() + self.xMovement
-        new_y = self.ycor() + self.yMovement
+        new_x = self.xcor() + self.xMovement * self.movementDirection
+        new_y = self.ycor() + self.yMovement * self.movementDirection
         self.goto(new_x, new_y)
         self.bounce()
         
-    def bounce(self):
-        if self.ycor() > 275 or self.ycor() < -275:
-            self.yMovement*= -1
+    def bounce(self):                                   
+        if self.ycor() > 275 or self.ycor() < -275:         # Detect collision with wall and bounce it off
+            self.yMovement*= -1             # multiply it w -1 to bounce it off
+        
+    def bouncePaddle(self):
+        self.xMovement*=-1
+        
+    def reset(self):
+        self.goto(0,0)
+        self.movementDirection *= -1
+        self.move()
+        
--- main.py
+++ main.py
@@ -14,7 +14,11 @@ leftPaddle = Paddle((-350,0))
 
 ball = Ball()
 
+rightPaddleScore = 0
+leftPaddleScore = 0
+
 screen.listen()
+
 screen.onkey(rightPaddle.moveUp, "Up")
 screen.onkey(rightPaddle.moveDown, "Down")
 
@@ -22,11 +26,22 @@ screen.onkey(leftPaddle.moveUp, "w")
 screen.onkey(leftPaddle.moveDown, "s")
 
 
-    
 gameIsOn = True
 while gameIsOn:
     time.sleep(0.069)
     screen.update()
     ball.move()
+    # Detect collision with paddles
+    if ball.distance(rightPaddle) < 50 and ball.xcor() > 320 or ball.distance(leftPaddle) < 50 and ball.xcor() < -320:
+        ball.bouncePaddle()
+    # Detect ball out of bounds 
+    # Right paddle miss
+    if ball.xcor() > 400:
+        rightPaddleScore += 1
+        ball.reset()
+    # Left paddle miss
+    elif ball.xcor() < -400:
+        leftPaddleScore += 1
+        ball.reset()
 
 screen.exitonclick()
